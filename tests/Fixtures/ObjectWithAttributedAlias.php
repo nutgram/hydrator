0b0d02519a7ccc5cd073793ac30c46b067fbba1b
Sergio Brighenti <SergiX44@users.noreply.github.com>
2022-04-25 11:23:53 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Fixtures/ObjectWithAttributedAlias.php
+++ tests/Fixtures/ObjectWithAttributedAlias.php
@@ -1,13 +1,11 @@
 <?php
 
-
 namespace SergiX44\Hydrator\Tests\Fixtures;
 
 use SergiX44\Hydrator\Annotation\Alias;
 
 final class ObjectWithAttributedAlias
 {
-
     #[Alias('non-normalized-value')]
     public string $value;
 }
