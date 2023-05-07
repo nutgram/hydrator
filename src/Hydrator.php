12b8487db5220f20a24605d9ffb25f6785f0ec82
Sergio Brighenti <sergio@brighenti.me>
2023-05-07 15:11:52 +0200

    hippity hoppity fix the array property

--- src/Hydrator.php
+++ src/Hydrator.php
@@ -20,7 +20,6 @@ use SergiX44\Hydrator\Annotation\ArrayType;
 use SergiX44\Hydrator\Annotation\ConcreteResolver;
 use SergiX44\Hydrator\Annotation\UnionResolver;
 use SergiX44\Hydrator\Exception\InvalidObjectException;
-
 use function array_key_exists;
 use function class_exists;
 use function ctype_digit;
@@ -36,7 +35,6 @@ use function is_string;
 use function is_subclass_of;
 use function sprintf;
 use function strtotime;
-
 use const FILTER_NULL_ON_FAILURE;
 use const FILTER_VALIDATE_BOOLEAN;
 use const FILTER_VALIDATE_FLOAT;
@@ -562,7 +560,8 @@ class Hydrator implements HydratorInterface
         }
 
         return array_map(function ($object) use ($arrayType) {
-            return $this->hydrate($arrayType->getInstance(), $object);
+            $newInstance = $this->container?->get($arrayType->class) ?? $arrayType->getInstance();
+            return $this->hydrate($newInstance, $object);
         }, $array);
     }
 
